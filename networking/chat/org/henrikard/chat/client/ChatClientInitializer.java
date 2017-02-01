24bc630ca1857284a749bfd506a6ef23808875ae
Rikard Fröberg <rikard@morus.se>
2017-02-02 00:19:11 +0100

    fixed indentation levels

--- networking/chat/org/henrikard/chat/client/ChatClientInitializer.java
+++ networking/chat/org/henrikard/chat/client/ChatClientInitializer.java
@@ -22,32 +22,32 @@ import java.net.Socket;
 
 public class ChatClientInitializer{
     
-    /*
-     * Hard coded server and port values.
-     * These could optionally be passed as arguments to main
-     * and forwarded here via the constructor.
-     */
-    private static final String SERVER="127.0.0.1";
-    private static final int PORT=1066;
-    private Socket socket;
-    public Socket getSocket(){
-        return socket;
+  /*
+   * Hard coded server and port values.
+   * These could optionally be passed as arguments to main
+   * and forwarded here via the constructor.
+   */
+  private static final String SERVER="127.0.0.1";
+  private static final int PORT=1066;
+  private Socket socket;
+  public Socket getSocket(){
+    return socket;
+  }
+  private void connect() throws IOException{
+    try{
+      socket=new Socket(SERVER,PORT);
+    }catch(IOException e){
+      System.err.println("Error connecting: " +e);
+      throw new IOException("Couldn't connect to the server", e);
     }
-    private void connect() throws IOException{
-        try{
-            socket=new Socket(SERVER,PORT);
-        }catch(IOException e){
-            System.err.println("Error connecting: " +e);
-	    throw new IOException("Couldn't connect to the server", e);
-        }
-    }
-    /**
-     * This constructor creates the initializer for the
-     * chat client and sets up the connection to the server.
-     * @throws IOException if the connection to the server fails
-     */
-    public ChatClientInitializer() throws IOException{
-        connect();
-        new Thread(new ChatGUI(this)).start();
-    }    
+  }
+  /**
+   * This constructor creates the initializer for the
+   * chat client and sets up the connection to the server.
+   * @throws IOException if the connection to the server fails
+   */
+  public ChatClientInitializer() throws IOException{
+    connect();
+    new Thread(new ChatGUI(this)).start();
+  }    
 }
