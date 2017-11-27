54c3890489fc98cbef37070a180194b1674bcaef
Rikard Fröberg <rikard@morus.se>
2017-11-27 01:14:06 +0100

    Google code style...

--- functional-streams/JGrep.java
+++ functional-streams/JGrep.java
@@ -3,20 +3,20 @@ import java.io.*;          // IOException, InputStreamReader, BufferedReader, Fi
 import java.nio.file.*;    // Files, Paths
 import java.util.stream.*; // Stream;
 import java.util.*;        // Arrays, List, ArrayList, Set, HashSet
-public class JGrep{
-  public static void main(String[] args){
+public class JGrep {
+  public static void main(String[] args) {
     final Env env = new Env(args);
-    if(!env.isValidArguments()){
+    if (!env.isValidArguments()) {
       usage();
     }
     Pattern pattern = env.pattern();
-    if(!env.readFromFiles()){
+    if (!env.readFromFiles()) {
       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
       in.lines()
         .filter(env.pattern().asPredicate())
         .forEach(s -> {System.out.println(s); env.incMatches();});
-    }else{
-      for(String file : env.files()){
+    } else {
+      for(String file : env.files()) {
         try (Stream<String> stream = Files.lines(Paths.get(file))) {
           stream.filter(env.pattern().asPredicate())
             // if there are more than one file in args, prefix each match with the file name:
@@ -29,37 +29,37 @@ public class JGrep{
     }
     System.exit(env.exitStatus());
   }
-  static class Env{
+  static class Env {
     private String[] args;
     private Set<String> badFiles = new HashSet<>();
     private int matches;
     public void incMatches(){ matches++; }
     public int numMatches() { return matches; }
-    public int exitStatus(){
+    public int exitStatus() {
       return badFiles.size()!=0?2:matches==0?1:0;
     }
-    public Env(String[] args){
+    public Env(String[] args) {
       this.args = args;
     }
-    public boolean isValidArguments(){
+    public boolean isValidArguments() {
       return args.length > 0;
     }
-    public boolean isCaseInsensitive(){
+    public boolean isCaseInsensitive() {
       return Arrays.asList(args).contains("-i");
     }
-    public Pattern pattern(){
-      if(isCaseInsensitive()){
+    public Pattern pattern() {
+      if (isCaseInsensitive()) {
         return Pattern.compile(regexp(), Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
-      }else{
+      } else {
         return Pattern.compile(regexp());
       }
     }
-    public String regexp(){
+    public String regexp() {
       String arg;
       String regexp=null;
-      for(int i=0;i<args.length;i++){
+      for(int i=0;i<args.length;i++) {
         arg = args[i];
-        if(arg.equals("-i")){
+        if(arg.equals("-i")) {
           continue;
         }
         // take the first non-option argument
@@ -68,22 +68,22 @@ public class JGrep{
       }
       return regexp;
     }
-    public List<String>files(){
+    public List<String>files() {
       ArrayList<String>files = new ArrayList<>();
       String arg;
       String regexp=null;
-      for(int i=0;i<args.length;i++){
+      for(int i=0;i<args.length;i++) {
         arg = args[i];
-        if(arg.equals("-i")){
+        if (arg.equals("-i")) {
           continue;
         }
-        if(regexp==null){
+        if (regexp==null) {
           regexp=arg;
-        }else{
-          if(new File(arg).exists()){
+        } else {
+          if (new File(arg).exists()) {
             files.add(arg);
-          }else{
-            if(!badFiles.contains(arg)){
+          } else {
+            if (!badFiles.contains(arg)) {
               System.err.println("JGrep: " + arg + ": No such file or directory");
               badFiles.add(arg);
             }
@@ -92,11 +92,11 @@ public class JGrep{
       }
       return files;
     }
-    private boolean readFromFiles(){
-      return files().size()!=0;
+    private boolean readFromFiles() {
+      return files().size() != 0;
     }
   }
-  private static void usage(){
+  private static void usage() {
     System.err.println("Usage: java JGrep [-i] PATTERN [FILE]...");
     System.err.println("where: ");
     System.err.println(" -i stands for case insensitive");
