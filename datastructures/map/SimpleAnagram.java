6d18a343e8ee69a80c698011b558000214d5f8a9
Rikard Fröberg <rikard@morus.se>
2017-12-03 17:52:18 +0100

    Fixed bug, added comments etc

--- datastructures/map/SimpleAnagram.java
+++ datastructures/map/SimpleAnagram.java
@@ -3,11 +3,19 @@ import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
 
+/**
+ * Reads a dictionary file and creates a datastructure with all anagrams
+ * found in the dictionary, as a Map&lt;String, Set&lt;String&gt;&gt .
+ * The key of the map is the words with their letters sorted.
+ */
 public class SimpleAnagram {
   
   private static final String DICTIONARY="words.txt";
   private Map<String, Set<String>> anagrams = new TreeMap<>();
-  
+
+  /** Creates a new SimpleAnagram object
+   * with the anagrams data structure created.
+   */
   public SimpleAnagram() {
     anagrams();
   }
@@ -18,19 +26,31 @@ public class SimpleAnagram {
       if (anagrams.containsKey(sorted)) {
         anagrams.get(sorted).add(word);
       } else {
-        anagrams.put(sorted, new TreeSet<String>());        
+        Set<String> set = new TreeSet<String>();
+        set.add(word);
+        anagrams.put(sorted, set);        
       }
     }
   }
 
+  /**
+   * Returns a list of all the anagram sets of at least <code>size</code>
+   * anagrams.
+   * @param size - the minimum size of the anagram sets to return
+   * @return A List&lt;Set&lt;String&gt;&gt; of all the anagram sets of
+   * at least <code>size</code>
+   */
   public List<Set<String>> foundAnagrams(int size) {
-    return anagrams.values().stream().filter(s -> s.size() >= size)
+    return anagrams.values().stream()
+      .sorted((s1, s2) -> s1.size() - s2.size())
+      .filter(s -> s.size() >= size)
       .collect(Collectors.toList());
   }
   
   private List<String> words() {
     try {
       return Files.lines(Paths.get(DICTIONARY))
+        .map(String::toLowerCase)
         .collect(Collectors.toList()); 
     } catch (IOException ioe) {
       System.err.println(ioe.getMessage());
@@ -43,7 +63,14 @@ public class SimpleAnagram {
     Arrays.sort(chars=s.toLowerCase().toCharArray());
     return new String(chars);
   }
-  
+
+  /**
+   * Returns a Set&lt;String&gt; of all the anagrams found for the given
+   * word, an empty Set for words not in the dictionary, and a Set of only
+   * the word itself, for words without anagrams.
+   * @param word The word to get the anagrams for
+   * @return A Set&lt;String&gt; with the found anagrams for the word.
+   */
   public Set<String> anagramsFor(String word) {    
     return anagrams.get(sortString(word));
   }
--- datastructures/map/TestSimpleAnagram.java
+++ datastructures/map/TestSimpleAnagram.java
@@ -1,11 +1,53 @@
+import java.util.Set;
+
+/**
+ * <p>Application showing the algorithm for finding anagrams
+ * put to practice.</p>
+ * <p>
+ * USAGE:
+ * java TestSimpleAnagram <word>...
+ * Where <word>... is the word(s) to find anagrams for.
+ * </p>
+ * <p>
+ * The program also lists the sets of at least 8 anagrams found in the
+ * dictionary.
+ * </p>
+ */
 public class TestSimpleAnagram {
 
-  public static void main(String[] args) {
-    SimpleAnagram sa = new SimpleAnagram();
-    String anagrams = sa.anagramsFor(args[0]).toString();
-    System.out.println(anagrams.equals("[]") ? "No anagrams found." : anagrams);
-    System.out.println("=====");
-    System.out.println("Words with at least 8 anagrams:");
-    System.out.println(sa.foundAnagrams(8));
+  private static void check(String[] args) {
+    if (args.length == 0) {
+      System.err.println("USAGE:");
+      System.err.println("java TestSimpleAnagram <word>...");
+      System.err.println("Where <word>... is the word(s) to " +
+                         "find anagrams for.");
+      System.exit(1);
+    }
   }
+  
+  public static void main(String[] args) {
+    // Verify that we have at least one argument
+    check(args);
+    // List the found anagrams for the words,
+    // and all 8-or-more anagram sets found in the dictionary
+    anagrams(args);
+  }
+
+  private static void anagrams(String[] words) {
+    SimpleAnagram sa = new SimpleAnagram();
+    System.out.println("====Anagrams====");
+    for (String word : words) {
+      Set<String> anagrams = sa.anagramsFor(word);
+      if (anagrams != null && anagrams.size() > 0) {
+        System.out.println("* Anagrams for " + word + ": " + anagrams);
+      } else {
+        System.out.println("* No anagrams for " + word + " found");
+      }
+    }
+    System.out.println("\n\n=================\n");
+    System.out.println("Words with at least 8 anagrams:");
+    for (Set<String> anagrams : sa.foundAnagrams(8)) {
+      System.out.println(anagrams.size() + " - " + anagrams);
+    }
+  }    
 }
