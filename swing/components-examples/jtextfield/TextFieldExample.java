2272e3b8509332f251af5121c2a6c456aadeb21d
Rikard Fröberg <rikard@morus.se>
2017-12-31 12:58:01 +0100

    Swing JTextField example

---
+++ swing/components-examples/jtextfield/TextFieldExample.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import static java.awt.Color.*;
+import java.awt.Color;
+
+public class TextFieldExample {
+
+  private JFrame window;
+  private JButton colorButton;
+  private JLabel prompt;
+  private ActionListener colorChanger;
+  private JTextField colorTextField;
+  
+  public TextFieldExample() {
+    initComponents();
+    layoutComponents();
+    addListeners();
+  }
+
+  private void initWindow() {
+    window = new JFrame("JTextField example");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setSize(800, 600);
+  }
+  private void initComponents() {
+    initWindow();
+    colorButton = new JButton("Change color");
+    prompt = new JLabel("Color in hex:");
+    colorTextField = new JTextField("000000");
+    colorTextField.selectAll();
+    colorTextField.requestFocus();
+    colorTextField.setToolTipText("<html><ul><li>Red: FF0000</li><li>Green: 00FF00</li><li>Blue: 0000FF</li></ul></html>");
+  }
+
+  private void layoutComponents() {
+    window.setLayout(new FlowLayout());
+    window.add(prompt);
+    window.add(colorTextField);
+    window.add(colorButton);
+  }
+
+  
+  
+  private void addListeners() {
+    colorChanger = e -> {
+      Color newColor = null;
+      try {
+        newColor = Color.decode("0x" + colorTextField.getText());
+      } catch (NumberFormatException ignore) {}
+      if (newColor != null) {
+        prompt.setForeground(newColor);
+      } else {
+        prompt.setForeground(BLACK);
+        colorTextField.setText("??????");
+        colorTextField.requestFocus();
+        colorTextField.selectAll();
+      }
+    };
+    colorTextField.addActionListener(colorChanger);
+    colorButton.addActionListener(colorChanger);
+  }
+  
+  public void show() {
+    window.setVisible(true);
+  }
+}
