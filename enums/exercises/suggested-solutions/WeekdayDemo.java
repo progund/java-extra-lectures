dcbeee8379477ca7a82553b0706787814a717723
Rikard Fröberg <rikard@morus.se>
2018-11-26 10:47:40 +0100

    Adds method investigating compareTo() on enums

--- enums/exercises/suggested-solutions/WeekdayDemo.java
+++ enums/exercises/suggested-solutions/WeekdayDemo.java
@@ -3,8 +3,23 @@ public class WeekdayDemo {
   public static void main(String[] args) {
     for (Weekday day : Weekday.values()) {
       testDay(day);
+      whichIsGreater(day);
     }
   }
+
+  private static void whichIsGreater(Weekday day) {
+    Weekday sat = Weekday.SATURDAY;
+    int result = day.compareTo(sat);
+    System.out.print(day + " is ");
+    if (result < 0) {
+      System.out.println("less than (before) " + sat);
+    } else if (result ==  0) {
+      System.out.println("the same as " + sat);
+    } else {
+      System.out.println("greater than (after) " + sat);
+    }
+  }
+
   private static void testDay(Weekday day) {
     System.out.print("It's " + day);
     if (day.isWeekday()) {
