4ac6721e7a532a746ec59830ebfe88d6b2d702c7
Rikard Fröberg <rikard@morus.se>
2018-11-26 16:09:33 +0100

    More client code using Optional

--- java-optionals/StudentDemo.java
+++ java-optionals/StudentDemo.java
@@ -1,5 +1,3 @@
-import java.util.Optional;
-
 public class StudentDemo {
   public static void main(String[] args) {
     Student aStudent = new Student("Charlie", "charlie@email.com");
@@ -7,6 +5,20 @@ public class StudentDemo {
 
     System.out.println(aStudent);
     System.out.println(another);
+
+    if (another.email().isPresent()) {
+      System.out.println("Here's the email for " + another.name() + ": "
+                         + another.email().get());
+    } else {
+      System.out.println(another.name() + " doesn't have an email.");
+    }
+    
+    if (aStudent.email().isPresent()) {
+      System.out.println("Here's the email for " + aStudent.name() + ": "
+                         + aStudent.email().get());
+    } else {
+      System.out.println(aStudent.name() + " doesn't have an email.");
+    }
     
   }
 }
